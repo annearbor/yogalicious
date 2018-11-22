ae3818e9c31dcdf346d0897ed6235d23939c567b
annearbor <betting.anne@gmail.com>
2018-11-22 22:30:18 +0100

    Iteration 2

--- canvas.js
+++ canvas.js
@@ -1,110 +1,199 @@
 // check if  jQuery is working
+
 $(function() {
-  console.log("DOM is Loaded");
-
-  var poses = {
-    downDog: {},
-    headstand: {},
-    warrior2: {},
-    warrior3: {}
-  };
-
   var canvas = document.getElementById("canvas");
   var ctx = canvas.getContext("2d");
 
-  poses.downDog.drawHead = function() {
-    var radius = 25; // Arc radius
-    var startAngle = 0; // Starting point on circle
-    var endAngle = Math.PI * 2; // End point on circle
-    var x = 420;
-    var y = 280;
-    ctx.moveTo(420, 280);
-    ctx.arc(x, y, radius, startAngle, endAngle, true);
-    ctx.stroke();
-  };
+  console.log("DOM is Loaded");
 
-  //DOWNDOG
+  var poses = [
+    {
+      name: "downdog",
+      buttonText: "Downward-facing Dog",
+      image: function() {
+        var radius = 20; // Arc radius
+        var startAngle = 0; // Starting point on circle
+        var endAngle = Math.PI * 2; // End point on circle
+        var x = 420;
+        var y = 270;
+        ctx.moveTo(420, 270);
+        ctx.arc(x, y, radius, startAngle, endAngle, true);
+        ctx.stroke();
+        ctx.beginPath();
+        ctx.moveTo(200, 300);
+        ctx.lineTo(150, 300); //foot
+        ctx.moveTo(150, 300);
+        ctx.lineTo(300, 150); //legs
+        ctx.lineTo(450, 300); //body
+        ctx.lineTo(480, 300); // hands
+        ctx.stroke();
+      }
+    },
+    {
+      name: "headstand",
+      buttonText: "Supported Headstand",
+      image: function() {
+        var radius = 20; // Arc radius
+        var startAngle = 0; // Starting point on circle
+        var endAngle = Math.PI * 2; // End point on circle
+        var x = 300;
+        var y = 300;
+        ctx.arc(x, y, radius, startAngle, endAngle, true);
+        ctx.stroke();
+        ctx.beginPath();
+        ctx.moveTo(300, 280);
+        ctx.lineTo(300, 100);
+        ctx.lineTo(280, 80);
+        ctx.moveTo(300, 240);
+        ctx.lineTo(260, 250);
+        ctx.moveTo(260, 250);
+        ctx.lineTo(240, 320);
+        ctx.lineTo(260, 330); //hands
+        ctx.stroke();
+      }
+    },
+    {
+      name: "warrior2",
+      buttonText: "Warrior II",
+      image: function() {
+        ctx.beginPath(); //body
+        ctx.moveTo(300, 150);
+        ctx.lineTo(300, 250);
+        ctx.moveTo(230, 200); //arms
+        ctx.lineTo(375, 200); //leg1
+        ctx.moveTo(300, 250);
+        ctx.lineTo(380, 250);
+        ctx.lineTo(390, 280);
+        ctx.lineTo(410, 278); //leg2
+        ctx.moveTo(300, 250);
+        ctx.lineTo(200, 300);
+        ctx.lineTo(210, 310);
+        var radius = 20; // Arc radius
+        var startAngle = 0; // Starting point on circle
+        var endAngle = Math.PI * 2; // End point on circle
+        var x = 300;
+        var y = 150;
+        ctx.moveTo(325, 160); // head
+        ctx.arc(x, y, radius, startAngle, endAngle, true);
+        ctx.stroke();
+      }
+    }
+  ];
 
-  poses.downDog.drawFeet = function() {
-    ctx.beginPath();
-    ctx.moveTo(200, 300);
-    ctx.lineTo(150, 300); //foot
-    ctx.stroke();
-  };
+  var extraPoses = [
+    {
+      name: "downdog",
+      buttonText: "Downward-facing Dog"
+    },
+    {
+      name: "headstand",
+      buttonText: "Supported Headstand"
+    },
+    {
+      name: "warrior2",
+      buttonText: "Warrior II"
+    },
+    {
+      name: "warrior1",
+      buttonText: "Warrior I"
+    },
+    {
+      name: "warrior3", //5
+      buttonText: "Warrior III"
+    },
+    {
+      name: "shoulderstand",
+      buttonText: "Supported Shoulderstand"
+    },
+    {
+      name: "tree",
+      buttonText: "Tree Pose"
+    },
+    {
+      name: "halfmoon",
+      buttonText: "Half-moonn Pose"
+    },
+    {
+      name: "mountain",
+      buttonText: "Mountain Pose"
+    },
+    {
+      name: "death", //10
+      buttonText: "Death Pose"
+    },
+    {
+      name: "wheel",
+      buttonText: "Wheel Pose"
+    }
+  ];
 
-  poses.downDog.drawLegs = function() {
-    ctx.beginPath();
-    ctx.moveTo(150, 300);
-    ctx.lineTo(300, 150); //legs
-    ctx.stroke();
-  };
+  var currentPoseArray = [];
+  var randomCurrentPose = Math.floor(Math.random() * poses.length);
+  var displayPose = poses[randomCurrentPose];
+  currentPoseArray.push(displayPose);
 
-  poses.downDog.drawBody = function() {
-    ctx.beginPath();
-    ctx.moveTo(300, 150);
-    ctx.lineTo(450, 300); //body
-    ctx.stroke();
-  };
+  displayPose.image();
 
-  poses.downDog.drawHands = function() {
-    ctx.beginPath();
-    ctx.moveTo(450, 300);
-    ctx.lineTo(500, 300); // hands
-    ctx.stroke;
-  };
+  var otherAnswersArray = extraPoses.filter(function(x) {
+    return displayPose.name !== x.name;
+  }); // create new copy of extraPoses array that does not have the pose that is already selected
+  console.log(otherAnswersArray);
+  for (var i = 0; i < otherAnswersArray.length; i++) {
+    if (currentPoseArray.length === 3) {
+      break;
+    } else {
+      var randomButton = Math.floor(Math.random() * otherAnswersArray.length);
+      currentPoseArray.push(otherAnswersArray[randomButton]);
+      otherAnswersArray.splice(randomButton, 1);
+    }
+  }
+  // console.log(otherAnswersArray);
+  //console.log(currentPoseArray);
 
-  // HEADSTAND
-  poses.headstand.drawHead = function() {
-    var radius = 20; // Arc radius
-    var startAngle = 0; // Starting point on circle
-    var endAngle = Math.PI * 2; // End point on circle
-    var x = 300;
-    var y = 300;
-    ctx.arc(x, y, radius, startAngle, endAngle, true);
-    ctx.stroke();
-  };
+  for (var i = 0; i < 3; i++) {
+    //take one away each time
+    //  if (i === 1) {
+    var randomIndex = Math.floor(Math.random() * currentPoseArray.length);
+    var randomButtonString = currentPoseArray[randomIndex];
+    var idName = "btn" + (i + 1);
+    document.getElementById(idName).innerText = randomButtonString.buttonText;
+    // }
 
-  poses.headstand.drawHands = function() {
-    ctx.beginPath();
-    ctx.lineTo(500, 300); // hands
-    ctx.stroke;
-  };
+    // $("#btn1").(randomButtonString.buttonText);
 
-  poses.headstand.drawHead = function() {
-    var radius = 20; // Arc radius
-    var startAngle = 0; // Starting point on circle
-    var endAngle = Math.PI * 2; // End point on circle
-    var x = 300;
-    var y = 300;
-    ctx.arc(x, y, radius, startAngle, endAngle, true);
-    ctx.stroke();
-  };
+    console.log(randomButtonString);
+    currentPoseArray.splice(randomIndex, 1);
+  }
 
-  poses.headstand.drawHands = function() {
-    ctx.beginPath();
-    ctx.moveTo(240, 320);
-    ctx.lineTo(280, 340); // hands
-    ctx.stroke;
-  };
-
-  poses.headstand.drawArms = function() {
-    ctx.beginPath();
-    ctx.moveTo(300, 250);
-    ctx.lineTo(260, 260);
-    ctx.lineTo(240, 320);
-    ctx.stroke();
-  };
-
-  poses.headstand.drawLegs = function() {
-    ctx.beginPath();
-
-    ctx.moveTo(300, 100);
-    ctx.lineTo(300, 280);
-    ctx.stroke();
-  };
-
-  poses.headstand.drawLegs();
-  poses.headstand.drawHands();
-  poses.headstand.drawArms();
-  poses.headstand.drawHead();
+  //splice(randomIndex, 1)
+  // $(".answer-buttons").append(randomButtonString);
 });
+
+// $(function() {
+//   // var gameStarted = false;
+//   // var canvas;
+//   // $('#start-game-button').click(function() {
+//   //     canvas = new starsCanvas();
+//   //     canvas.drawStars();
+//   //     canvas.drawSchwertfisch();
+//   //     console.log('--- drawn the board ---');
+
+//   // zufällige Reihenfolge der buttons
+//   // ausschließen dass die richtige antwort als auch falsche da ist
+//   // verschiedene Bilder/Posen
+//   var randomIndex = Math.floor(Math.random() * answerArray.length); // random position in array ( _.sample )
+//   var buttonString = answerArray[randomIndex]; // <button......
+//   $(".questionOne").append(buttonString);
+//   var randomIndex = Math.floor(Math.random() * answerArray.length); // random position in array ( _.sample )
+//   var buttonString = answerArray[randomIndex]; // <button......
+//   $(".questionOne").append(buttonString);
+//   $(".questionOne").append(answerObject.Schwertfisch);
+//   $(".questionOne button").click(function() {
+//     if ($(this).attr("data-stellar-constellation") === "Schwertfisch")
+//       alert("Juhu!");
+//   });
+//   // hides the start button
+//   $("#start-game-button").hide();
+//   $(".questionOne").toggle();
+//   gameStarted = true;
+// });
